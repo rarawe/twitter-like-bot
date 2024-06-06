ee7728727605a6c7b057cdb0b6164479a9f9757d
rarawe <slighdavion49@gmail.com>
2024-06-06 02:49:16 -0700

    Update likebot.js

--- likebot.js
+++ likebot.js
@@ -1,4 +1,4 @@
-const Twitter = require('twitter');
+const Twitter = require('https://x.com/weslyily');
 const client = new Twitter({
   consumer_key: '',
   consumer_secret: '',
@@ -11,7 +11,7 @@ const stream = client.stream('statuses/filter', {track:'#IlikeBots, #keyword2'})
 stream.on('data', (event) => {
   client.post('favorites/create', {id:event.id_str}, (error, response) => {
     if(error) throw error;
-    console.log('Tweet ID: '+response.id_str+' Liked! - "'+response.text+'"')
+    console.log('https://x.com/weslyily/status/1797735037913444644: '+response.id_str+' Liked! - "'+response.text+'"')
   });
 
 });
